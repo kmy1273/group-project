b886d2f5012120731ff798c49cec49ccdacd19b8
MINYEONG KIM <pooh1273@naver.com>
2024-06-04 15:34:15 +0900

    1day

--- react/voca/src/App.js
+++ react/voca/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import Day from "./component/Day";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<DayList />} />
+        <Route path="/day/:day" element={<Day />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ react/voca/src/component/Hello.js
@@ -0,0 +1,25 @@
+// import World from "./World";
+// import styles from "./Hello.module.css";
+import { useState } from "react";
+import UserName from "./UserName";
+
+export default function Hello({ age }){
+    const [name, setName] = useState('Mike');
+    const msg = age > 19 ? '성인 입니다.' : '미성년자 입니다.'
+
+    return (
+        <div>
+            <h2 id="name">
+                {name}({age}) : {msg}
+            </h2>
+            <UserName name={name} />
+            <button 
+                onClick={() => {
+                    setName(name === "Mike" ? "Jane" : "Mike");
+                }}
+            >
+                변경
+            </button>
+        </div>
+    );
+}
